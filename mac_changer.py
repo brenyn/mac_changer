ae6f44e091d8e9a16614011dcbf95638835abc48
Brenyn <brenynk@gmail.com>
2020-05-19 21:51:24 -0400

    added error handling

--- mac_changer.py
+++ mac_changer.py
@@ -20,7 +20,14 @@ def get_arguments():
 	parser = optparse.OptionParser()
 	parser.add_option("-i", "--interface" , dest="interface", help="Interface having MAC changed")
 	parser.add_option("-m", "--mac", dest="desiredMAC", help="Desired MAC address")
-	return parser.parse_args()
+	(options,arguments) = parser.parse_args()
+
+	if not (options.interface):
+		parser.error("ERROR: Please specify an interface, use -h or --help for more info.")
+	elif not (options.desiredMAC):
+		parser.error("ERROR: Please specify a MAC address, use -h or --help for more info.")
+	else:
+		return options
 
 def change_mac(interface,desiredMAC):
 	print("[+] Changing MAC address for " + interface + " to " + desiredMAC)
@@ -31,6 +38,6 @@ def change_mac(interface,desiredMAC):
 
 #original MAC for eth0 = 08:00:27:23:ff:90 test MAC address 00:11:22:33:44:55
 
-(options,arguments) = get_arguments()
+options = get_arguments()
 
 change_mac(options.interface, options.desiredMAC)
