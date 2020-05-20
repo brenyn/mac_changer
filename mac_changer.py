7bc0947647d3d60462b7c238367660584e526ae4
Brenyn <brenynk@gmail.com>
2020-05-19 20:15:08 -0400

    added basic user input

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,11 @@
 
 import subprocess
 
-subprocess.call('sudo ifconfig eth0 down', shell = True)
-subprocess.call('sudo ifconfig eth0 hw ether 08:00:27:23:ff:90', shell = True)
-subprocess.call('sudo ifconfig eth0 up', shell = True)
+interface = input("Interface = ")
+desiredMAC = input("New MAC address = ") 	#original MAC for eth0 = 08:00:27:23:ff:90 test MAC address 00:11:22:33:44:55
+
+print("[+] Changing MAC address for " + interface + " to " + desiredMAC)
+
+subprocess.call('sudo ifconfig ' + interface + ' down', shell = True)
+subprocess.call('sudo ifconfig ' + interface + ' hw ether ' + desiredMAC, shell = True)
+subprocess.call('sudo ifconfig ' + interface + ' up', shell = True)
