9e282d8dab58d9e5c42ecd1c277adb5061ff5693
Brenyn <brenynk@gmail.com>
2020-05-19 23:50:33 -0400

    print ifconfig upon completion

--- mac_changer.py
+++ mac_changer.py
@@ -21,18 +21,21 @@ def get_arguments():
 	parser.add_option("-i", "--interface" , dest="interface", help="Interface having MAC changed")
 	parser.add_option("-m", "--mac", dest="desiredMAC", help="Desired MAC address")
 	(options,arguments) = parser.parse_args()
+	# options: an object containing values for all of your options
+	# arguments: the list of arguments to process / remaining arguments (unfilled) after parsing options
+	# https://docs.python.org/2/library/optparse.html
 
-	if not (options.interface):
-		parser.error("ERROR: Please specify an interface, use -h or --help for more info.")
-	elif not (options.desiredMAC):
-		parser.error("ERROR: Please specify a MAC address, use -h or --help for more info.")
+	if not (options.interface): # if an interface is not submitted
+		parser.error("Please specify an interface, use -h or --help for more info.")
+	elif not (options.desiredMAC): # or if a MAC address is not submitted
+		parser.error("Please specify a MAC address, use -h or --help for more info.")
 	else:
 		return options
 
 def change_mac(interface,desiredMAC):
 	print("[+] Changing MAC address for " + interface + " to " + desiredMAC)
 
-	subprocess.call(['sudo', 'ifconfig', interface, 'down'])
+	subprocess.call(['sudo', 'ifconfig', interface, 'down']) #executes as -> sudo ifconfig "interface" down
 	subprocess.call(['sudo', 'ifconfig', interface, 'hw', 'ether', desiredMAC])
 	subprocess.call(['sudo', 'ifconfig', interface, 'up'])
 
@@ -41,3 +44,6 @@ def change_mac(interface,desiredMAC):
 options = get_arguments()
 
 change_mac(options.interface, options.desiredMAC)
+
+ifconfig_result = subprocess.check_output(["sudo", "ifconfig", options.interface])
+print(ifconfig_result)
